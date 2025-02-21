702dbfabe9bc4e01020897b490ae60ba353ed26c
TheAceMotiur <motiurop@outlook.com>
2025-02-21 14:02:21 +0300

    Update API key in configuration file for security improvement

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 // API Configuration
 define('API_URL', 'https://onenetly.com/api.php');
-define('API_KEY', '59d144a41422d3f7881fefdae6ffd53b020264f57a5c7da10bd1345bb54e6fb1'); //Register https://onenetly.com and get api key
+define('API_KEY', 'your_api_key'); //Register https://onenetly.com and get api key
 
 // Site Configuration
 define('SITE_NAME', 'FreeNetly');
